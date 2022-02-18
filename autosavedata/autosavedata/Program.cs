1507cb61c707426656ac6dc5384fda38b8b9bb8b
Satyam <raghuvanshisatyam2@gmail.com>
2022-02-18 12:10:06 +0530

    Code update on 18022022

---
+++ autosavedata/autosavedata/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autosavedata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            var startTimeSpan = TimeSpan.Zero;
+            var periodTimeSpan = TimeSpan.FromSeconds(15);
+            var timer = new System.Threading.Timer((e) =>
+            {
+                Random rnd = new Random();
+                long num = rnd.Next();
+                var dend = new Sendnumber();
+                dend.SavenumberAuto(num);
+            }, null, startTimeSpan, periodTimeSpan);
+
+            Console.Read();
+           
+        }
+    }
+}
---
+++ autosavedata/autosavedata/Sendnumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autosavedata
+{
+  public class Sendnumber
+    {
+        public void SavenumberAuto(long number)
+        {
+            using (DashboardProgEntities dashboard = new DashboardProgEntities())
+            {
+                var num = new Number()
+                {
+                    Number1 = number
+                };
+                dashboard.Numbers.Add(num);
+                dashboard.SaveChanges();
+            }
+        }
+
+    }
+}
